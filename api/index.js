9566f343e6613570179b68c703e852f970369dd9
Baon2024 <Baon2020@protonmail.com>
2025-01-14 03:35:35 +0000

    extra stuff

--- api/index.js
+++ api/index.js
@@ -40,12 +40,12 @@ const endpointSecret = 'whsec_d841e887e13b7130ce9da8227aafc1a2c38c9289b03f48f955
 
 // Middleware
 //app.use(bodyParser.json());
-//app.use(cors());
+app.use(cors({credentials: true, /* Allow cookies to be sent*/}));
 // Enable CORS for specific origin (your frontend URL)
-app.use(
+/*app.use(
   cors({
     origin: 'https://experimentalformalbridge-l5q997h6c.vercel.app',
-  })
+  })*/
 );
 app.use('/webhook', bodyParser.raw({ type: 'application/json' }));
 
